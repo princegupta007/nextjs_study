cdb1798e7ee51500fb14cf439d8bf574909ad9c4
Prince Gupta <princegupta98299@gmail.com>
2025-01-09 20:34:50 +0530

    Initial project setup

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>About
+
+
+SENTRY_AUTH_TOKEN=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
+    </div>
   )
+  
 }
 
 export default About
